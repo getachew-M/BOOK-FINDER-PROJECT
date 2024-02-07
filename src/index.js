69f7496072e5089fec15b4519187db519621524a
Getachew <triplets3e@gmail.com>
2024-02-07 12:07:10 -0500

    Front end updats

--- src/index.js
+++ src/index.js
@@ -3,26 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< Updated upstream
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import { AuthProvider } from './Context/AuthContext';
-=======
->>>>>>> Stashed changes
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< Updated upstream
     <AuthProvider>
     <Router>
       <App />
     </Router>
     </AuthProvider>
-  
-=======
-    <App />
->>>>>>> Stashed changes
+
   </React.StrictMode>
 );
 
